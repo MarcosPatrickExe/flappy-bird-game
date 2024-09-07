e719856b7b8f4eceee27447ab0a4f4e0f06adadf
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-09-06 23:11:17 -0300

    Update CliqueBotao.java

--- src/playGame/CliqueBotao.java
+++ src/playGame/CliqueBotao.java
@@ -1,5 +1,5 @@
 package playGame;
-import java.awt.event.MouseEvent;
+import java.awt.event.MouseEvent; 
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
