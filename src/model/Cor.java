e7d510dcd1ed7e4af96a393904ad712f9f3ef326
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-09-24 22:28:25 -0300

    Update Cor.java

--- src/model/Cor.java
+++ src/model/Cor.java
@@ -30,7 +30,7 @@ public class Cor{
     
     public static Cor rgb(int r, int g, int b) {
         int indice = (r << 16) | (g << 8) | b;
-        if(!cores.containsKey(indice)) { 
+        if(!cores.containsKey(indice)) {  
             cores.put(indice, new Cor(r, g, b));
         }
         return cores.get(indice);
