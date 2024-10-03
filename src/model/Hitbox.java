a9f925d47b2fb7d5e22e2e99f78718441717efbf
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-10-02 22:25:16 -0300

    Update Hitbox.java

--- src/model/Hitbox.java
+++ src/model/Hitbox.java
@@ -11,7 +11,7 @@ public class Hitbox {
     public Hitbox(double x0, double y0, double x1, double y1) {
         this.x0 = x0;
         this.x1 = x1;
-        this.y0 = y0; 
+        this.y0 = y0;  
         this.y1 = y1; 
     }
     
