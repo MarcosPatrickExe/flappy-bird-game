769e4260d38ee688b1efe01f99bfbea0d86c54ba
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-10-03 21:50:29 -0300

    Update Jogo.java

--- src/model/Jogo.java
+++ src/model/Jogo.java
@@ -5,7 +5,7 @@ import playGame.Tela;
 //CLASSE ABSTRATA QUE SERVIRÁ DE REFERÊNCIA PARA A CLASSE PRINCIPAL DE EXECUÇÃO DO JOGO, A CLASSE "FlappyBird"
 abstract public class Jogo {
     abstract public String getTitulo();
-    abstract public int getLargura(); 
+    abstract public int getLargura();  
     abstract public int getAltura();
     abstract public void tique(double dt);
    // abstract public void tique(java.util.Set<String> teclas, double dt);
