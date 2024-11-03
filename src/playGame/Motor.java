fa431800fd0af9fd679e664f0ed6e2c97098d1ab
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-11-03 15:56:48 -0300

    Update Motor.java

--- src/playGame/Motor.java
+++ src/playGame/Motor.java
@@ -4,7 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D; 
-import java.awt.event.KeyEvent;
+import java.awt.event.KeyEvent; 
 import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
