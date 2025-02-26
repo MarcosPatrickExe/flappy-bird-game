56737b4cf054dad91f0dc53a774591e280341039
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2025-02-26 18:08:20 -0300

    Update Motor.java

--- src/playGame/Motor.java
+++ src/playGame/Motor.java
@@ -9,7 +9,7 @@ import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent; 
 import java.awt.image.BufferStrategy;
-import java.util.TreeSet;
+import java.util.TreeSet; 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.GraphicsDevice;
