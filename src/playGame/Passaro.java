b6d1b66ab04fea549500de8177346240089de162
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2025-11-28 23:52:04 -0300

    Update Passaro.java

--- src/playGame/Passaro.java
+++ src/playGame/Passaro.java
@@ -6,7 +6,7 @@ import playGame.FlappyBird.GameState;
   
 /**
  * @author Patrick    
- */ 
+*/ 
   
 public class Passaro implements IPassaro {  
     double x, y;
@@ -86,6 +86,7 @@ public class Passaro implements IPassaro {
 
  //link do codigo original:
  //https://www.dropbox.com/s/a2wy4bsskrpugq2/Flappy%20Bird.rar?dl=
+
 
 
 
