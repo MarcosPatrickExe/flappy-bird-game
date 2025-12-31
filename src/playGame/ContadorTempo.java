df44919d31f6b9dd0fde2c50f638f1b785d570a1
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2025-12-30 22:38:59 -0300

    Update ContadorTempo.java

--- src/playGame/ContadorTempo.java
+++ src/playGame/ContadorTempo.java
@@ -2,7 +2,7 @@ package playGame;
 import java.lang.*;
    
 /**
- * @author Patrick    
+ *  @author Patrick    
  */   
  
 class ContadorTempo implements Runnable{ 
@@ -30,6 +30,7 @@ class ContadorTempo implements Runnable{
        
     } 
 }
+
 
 
 
